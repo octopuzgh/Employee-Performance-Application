afba149862d5b4e1935a986fa4d3fe882dec6b73
octopuzgh <2632997649@qq.com>
2026-04-10 17:05:04 +0800

    scripts： Add database and Spark configuration settings

---
+++ scripts/config/settings.py
@@ -0,0 +1,26 @@
+
+# MySQL 配置
+MYSQL_CONFIG = {
+    "url": "jdbc:mysql://192.168.152.131:3306/employee_performance?useSSL=false",
+    "user": "octopuz_remote",
+    "password": "Octopuz@123",
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+
+# Spark 配置
+SPARK_CONFIG = {
+    "appName": "绩效统计API",
+    "master": "local[*]",
+    "config": {
+        "spark.sql.adaptive.enabled": "true",
+        "spark.sql.adaptive.coalescePartitions.enabled": "true"
+    }
+}
+
+# JDBC 通用配置（不含表名）
+JDBC_CONFIG = {
+    "url": MYSQL_CONFIG["url"],
+    "user": MYSQL_CONFIG["user"],
+    "password": MYSQL_CONFIG["password"],
+    "driver": MYSQL_CONFIG["driver"]
+}
